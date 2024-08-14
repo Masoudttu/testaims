12404a05fed6419452c8eda927a24058ba0e585e
Masoudttu <164294274+Masoudttu@users.noreply.github.com>
2024-08-14 14:33:19 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,84 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "603ilnviprl":
+        Script1();
+        break;
+      case "6kWA2SpdCv2":
+        Script2();
+        break;
+      case "5w7XQfSMIPk":
+        Script3();
+        break;
+      case "6gl655yjMrD":
+        Script4();
+        break;
+      case "6EpOdLPDyCv":
+        Script5();
+        break;
+      case "5lovJXC6wmP":
+        Script6();
+        break;
+      case "5shL0JT12Sv":
+        Script7();
+        break;
+      case "6cACmVb56zG":
+        Script8();
+        break;
+      case "6V40H0oTXS0":
+        Script9();
+        break;
+      case "6rntviyYySB":
+        Script10();
+        break;
+      case "67BG5PYJEya":
+        Script11();
+        break;
+      case "6PtNuc3N4zt":
+        Script12();
+        break;
+      case "6bxPmTaWZKM":
+        Script13();
+        break;
+      case "5VlMzssPgM9":
+        Script14();
+        break;
+      case "5bWZKPGtu2A":
+        Script15();
+        break;
+      case "5hyH2wlSWjW":
+        Script16();
+        break;
+      case "5pMGFwH0AEQ":
+        Script17();
+        break;
+      case "6V0VLsmePmG":
+        Script18();
+        break;
+      case "6pPedxZw44K":
+        Script19();
+        break;
+      case "6pb5cp7MrN4":
+        Script20();
+        break;
+      case "5WNBHHapCle":
+        Script21();
+        break;
+      case "64Y5O1rDu2z":
+        Script22();
+        break;
+      case "6awJCF8zclR":
+        Script23();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
